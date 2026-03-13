05466ec30badeae5a92a160fbfb36cf2654f8e65
Lastorder <18280396+Lastorder-DC@users.noreply.github.com>
2026-03-13 13:36:41 +0900

    Version 1.15.0

--- js/chatassist.js
+++ js/chatassist.js
@@ -3,8 +3,8 @@
  *  / /   / __ \/ __ `/ __/ /| | / ___/ ___/ / ___/ __/   / 
  * / /___/ / / / /_/ / /_/ ___ |(__  |__  ) (__  ) /_/   |  
  * \____/_/ /_/\__,_/\__/_/  |_/____/____/_/____/\__/_/|_|  
- *                 V E R S I O N    1.14.0.0
- *       Last updated by Lastorder-DC on 2026-03-05.
+ *                 V E R S I O N    1.15.0
+ *       Last updated by Lastorder-DC on 2026-03-13.
  */
 // 변수 초기화
 window.chat = {};
@@ -21,7 +21,7 @@ window.cimesocket = {};
 window.cimesocket.isInited = false;
 
 // 버전 번호
-window.chat.version = "1.14.0.0";
+window.chat.version = "1.15.0";
 
 // 채팅 관련 설정 변수
 window.chat.template = null;
